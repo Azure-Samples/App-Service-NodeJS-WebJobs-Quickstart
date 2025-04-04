bbf9a38434956931a88d5e1dee172e5cf5ecef3e
denverbrittain <dbrittain@microsoft.com>
2025-04-04 18:09:04 -0500

    initial commit - webjob nodejs sample for app service linux

---
+++ webjob.js
@@ -0,0 +1,8 @@
+// Import the 'Date' object from JavaScript
+const currentTime = new Date();
+
+// Format the time as a string
+const formattedTime = currentTime.toLocaleTimeString();
+
+// Output the formatted time to the console
+console.log(`Current system time is: ${formattedTime}`);
